53edfd20471146230dbca54d2d3d8cda28b111d3
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-26 11:28:59 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,29 @@
+class Funcionario {
+    private String nome;
+    private int id;
+
+    public Funcionario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public void baterPonto() {
+        System.out.println(nome + " bateu o ponto.");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
